8ee9a5b877814919997474e06e34acf3286fff84
Akanksha10029 <akanksharani405@gmail.com>
2024-10-20 21:47:25 +0530

    hello world first code

---
+++ HelloWorld/hello.js
@@ -0,0 +1,9 @@
+// to import the math file, we use the require function
+const math = require('./math');
+// To get all the functions present in the math file, they will all get displayed
+console.log(math)
+
+// To get the sum of the two numbers
+console.log(math.add(24,50))
+
+console.log("Hello Node Js");
